9290fdf5d77027828f26234e307fd5cee65d22d2
nana <nana.koranteng@principia.edu>
2025-10-26 20:01:38 -0500

    feat(client): initial Vite React + TS + MUI + Redux Toolkit scaffold

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
+);
---
+++ client/src/theme.ts
@@ -0,0 +1,5 @@
+import { createTheme } from '@mui/material/styles'
+export default createTheme({
+  palette: { primary: { main: '#173b7e' }, secondary: { main: '#FFD700' } },
+  shape: { borderRadius: 12 },
+})
